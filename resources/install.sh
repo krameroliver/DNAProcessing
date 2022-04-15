004db624f9402e157bc79f4950a5a2b243d218be
krameroliver <krameroliver1@gmail.com>
2022-04-15 19:00:00 +0200

    Create install.sh

---
+++ resources/install.sh
@@ -0,0 +1,12 @@
+  apt-get update && apt install -y python3-pip &&
+  pip3 install biopython &
+  pip3 install pika &
+  pip3 install pandas &
+  pip3 install pymongo &
+  pip3 install pymysql &
+  pip3 install split-fasta &
+  pip3 install pyYaml &
+  pip3 install mysql-connector-python &
+  pip3 install scikit-learn &
+  pip3 install Levenshtein &
+  pip3 install matplotlib
